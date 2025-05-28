9991f571fc61eca8278422581b5ceb1c6b210b01
Hood12345 <hcosubmissions@gmail.com>
2025-05-28 19:12:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory, jsonify, abort
+from flask import Flask, request, send_from_directory, jsonify, abort, render_template_string
 from werkzeug.utils import secure_filename
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -6,6 +6,7 @@ import os
 import uuid
 import time
 import magic
+import threading
 
 # --- Config ---
 UPLOAD_FOLDER = os.path.join(os.getcwd(), "static")
@@ -87,12 +88,42 @@ def serve_static(filename):
             os.remove(file_path)
         abort(410, description="File has expired")
 
+    # Render a UI with a download link in case auto-download doesn't work
+    download_html = f'''
+    <!DOCTYPE html>
+    <html><head><meta charset="UTF-8"><title>Download File</title></head>
+    <body style="text-align:center;margin-top:40px;">
+      <h2>Your file is ready to download</h2>
+      <a href="/file-download/{filename}" download>Click here to download</a>
+    </body></html>
+    '''
+    return render_template_string(download_html)
+
+# --- Actual forced download ---
+@app.route('/file-download/<path:filename>', methods=['GET'])
+def file_download(filename):
     return send_from_directory(
         app.config['UPLOAD_FOLDER'],
         filename,
         as_attachment=True,
         download_name=f"download.{filename.rsplit('.', 1)[1]}"
     )
+
+# --- Cleanup expired files in background ---
+def cleanup_expired_files():
+    while True:
+        time.sleep(300)  # Check every 5 minutes
+        for fname in os.listdir(UPLOAD_FOLDER):
+            if "__" in fname:
+                try:
+                    _, expiry = extract_filename_parts(fname)
+                    if time.time() > expiry:
+                        os.remove(os.path.join(UPLOAD_FOLDER, fname))
+                except Exception:
+                    continue
+
+cleanup_thread = threading.Thread(target=cleanup_expired_files, daemon=True)
+cleanup_thread.start()
 
 # --- Healthcheck ---
 @app.route('/', methods=['GET'])
